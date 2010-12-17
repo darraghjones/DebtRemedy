8b72714033d4551b011e6929b6fe4ed35bfbd135
Darragh Jones <darragh.jones@gmail.com>
2010-12-17 21:44:27 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DebtRemedy::Application.config.secret_token = 'deee36639a71ca275f9a9520acbfb3d5e55821762e64aa9f02105c9848031bbee249151acda8fd31a3dbbf19c55b9ce65f29705b609ee6501eff249aa425d297'
